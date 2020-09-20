5eb5748db2fce5bf116fd2ca1faedc3a342089ae
Evaldo Expedito dos Santos <eesantosap@hotmail.com>
2020-09-19 22:27:04 -0300

    terceira versão

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel do Expedito
+                    Laravel do Expedito 2
                 </div>
 
                 <div class="links">
